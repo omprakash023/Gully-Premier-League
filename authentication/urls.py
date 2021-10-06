6b9be191f7e26a05f0c3f72830026b0dc0571ff4
DK0264@digikull.com <omprakashvishwkarma019@gmail.com>
2021-10-06 17:09:50 +0530

    BE implementation of Forgot Password

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path, include
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
 from .views import index
 
 urlpatterns = [
     path('', index),
+    path('password_reset/',auth_views.PasswordResetView.as_view(),name='password_reset'),
+    path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
+    path('reset/done/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
+   
    
 ]
